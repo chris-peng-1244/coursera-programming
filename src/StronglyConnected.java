1fb6bffd4204180c90804af3b99ab5c3877f5313
ChrisPeng <pengchi1244@gmail.com>
2018-05-24 18:05:10 +0800

    Finsih DAG sort and SSC count

--- src/StronglyConnected.java
+++ src/StronglyConnected.java
@@ -1,28 +1,82 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 public class StronglyConnected {
-    private static int numberOfStronglyConnectedComponents(ArrayList<Integer>[] adj) {
-        //write your code here
-        return 0;
+  private static int clock = 1;
+  private static int numberOfStronglyConnectedComponents(ArrayList<Integer>[] adj) {
+    ArrayList<Integer>[] reversedGraph = reverseGraph(adj);
+    int[] used = new int[adj.length];
+    Map<Integer, Integer> postOrders = dfs(reversedGraph, used);
+    List<Integer> ordersReversed = new ArrayList<>(postOrders.keySet());
+    ordersReversed.sort(Collections.reverseOrder());
+    used = new int[adj.length];
+    int componentCounter = 0;
+    for (Integer order: ordersReversed) {
+      int vertex = postOrders.get(order);
+      if (used[vertex] == 0) {
+        simpleExplore(adj, vertex, used);
+        componentCounter++;
+      }
     }
+    return componentCounter;
+  }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
-        }
-        for (int i = 0; i < m; i++) {
-            int x, y;
-            x = scanner.nextInt();
-            y = scanner.nextInt();
-            adj[x - 1].add(y - 1);
-        }
-        System.out.println(numberOfStronglyConnectedComponents(adj));
+  private static Map<Integer, Integer> dfs(ArrayList<Integer>[] adj, int[] used) {
+    Map<Integer, Integer> orders = new HashMap<>();
+    for (int i = 0; i < adj.length; i++) {
+      if (used[i] == 0) {
+        explore(adj, i, used, orders);
+      }
     }
+    return orders;
+  }
+
+  private static void explore(ArrayList<Integer>[] adj, int i, int[] used, Map<Integer, Integer> orders) {
+    used[i] = 1;
+    for (Integer v: adj[i]) {
+      if (used[v] == 0) {
+        explore(adj, v, used, orders);
+      }
+    }
+    orders.put(clock++, i);
+  }
+
+  private static void simpleExplore(ArrayList<Integer>[] adj, int i, int[] used) {
+    used[i] = 1;
+    for (Integer v: adj[i]) {
+      if (used[v] == 0) {
+        simpleExplore(adj, v, used);
+      }
+    }
+  }
+
+  private static ArrayList<Integer>[] reverseGraph(ArrayList<Integer>[] adj) {
+    ArrayList<Integer>[] reversedGraph = (ArrayList<Integer>[])new ArrayList[adj.length];
+    for (int i = 0; i < adj.length; i++) {
+      reversedGraph[i] = new ArrayList<>();
+    }
+    for (int i = 0; i < adj.length; i++) {
+      for (Integer v: adj[i]) {
+        reversedGraph[v].add(i);
+      }
+    }
+    return reversedGraph;
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    int m = scanner.nextInt();
+    ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+    for (int i = 0; i < n; i++) {
+      adj[i] = new ArrayList<Integer>();
+    }
+    for (int i = 0; i < m; i++) {
+      int x, y;
+      x = scanner.nextInt();
+      y = scanner.nextInt();
+      adj[x - 1].add(y - 1);
+    }
+    System.out.println(numberOfStronglyConnectedComponents(adj));
+  }
 }
 
--- src/Toposort.java
+++ src/Toposort.java
@@ -1,37 +1,59 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 public class Toposort {
-    private static ArrayList<Integer> toposort(ArrayList<Integer>[] adj) {
-        int used[] = new int[adj.length];
-        ArrayList<Integer> order = new ArrayList<Integer>();
-        //write your code here
-        return order;
-    }
+  private static int clock = 1;
 
-    private static void dfs(ArrayList<Integer>[] adj, int[] used, ArrayList<Integer> order, int s) {
-      //write your code here
+  private static ArrayList<Integer> toposort(ArrayList<Integer>[] adj) {
+    int used[] = new int[adj.length];
+//    ArrayList<Integer> order = new ArrayList<Integer>();
+    Map<Integer, Integer> order = dfs(adj, used);
+    List<Integer> orderValues = new ArrayList<>(order.keySet());
+    Collections.sort(orderValues, Collections.reverseOrder());
+    ArrayList<Integer> result = new ArrayList<>();
+    for (int orderValue: orderValues) {
+      result.add(order.get(orderValue));
     }
+    return result;
+  }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
-        }
-        for (int i = 0; i < m; i++) {
-            int x, y;
-            x = scanner.nextInt();
-            y = scanner.nextInt();
-            adj[x - 1].add(y - 1);
-        }
-        ArrayList<Integer> order = toposort(adj);
-        for (int x : order) {
-            System.out.print((x + 1) + " ");
-        }
+  private static Map<Integer, Integer> dfs(ArrayList<Integer>[] adj, int[] used) {
+    Map<Integer, Integer> order = new HashMap<>();
+    for (int i = 0; i < adj.length; i++) {
+      if (used[i] == 0) {
+        explore(adj, i, used, order);
+      }
     }
+    return order;
+  }
+
+  private static void explore(ArrayList<Integer>[] adj, int v, int[] used, Map<Integer, Integer> order) {
+    used[v] = 1;
+    for (Integer neighbour: adj[v]) {
+      if (used[neighbour] == 0) {
+        explore(adj, neighbour, used, order);
+      }
+    }
+    order.put(clock++, v);
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    int m = scanner.nextInt();
+    ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
+    for (int i = 0; i < n; i++) {
+      adj[i] = new ArrayList<Integer>();
+    }
+    for (int i = 0; i < m; i++) {
+      int x, y;
+      x = scanner.nextInt();
+      y = scanner.nextInt();
+      adj[x - 1].add(y - 1);
+    }
+    ArrayList<Integer> order = toposort(adj);
+    for (int x : order) {
+      System.out.print((x + 1) + " ");
+    }
+  }
 }
 
